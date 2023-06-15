50bcc4a2ab6fb5495eb9a79c3f5bc55d66f534bb
MoochangPark <119276371+MoochangPark@users.noreply.github.com>
2023-06-15 15:38:38 +0900

    [Silver I] Title: 팔, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.py
@@ -0,0 +1,16 @@
+l, r = map(str, input().split(' '))
+
+cnt = 0
+
+if len(l) != len(r):
+    print(cnt)
+
+else:
+    for i in range(len(l)):
+        if l[i] == r[i]:
+            if l[i]== '8':
+                cnt += 1
+        else:
+            break
+            
+    print(cnt)
